fbdc4c67f0107c8d9b4c977e49363703b48ba180
Ramón García Fernández <ramon.garcia.f@gmail.com>
2016-06-01 02:19:46 +0200

    Finish adaptation in CustomMapping.hs to megaparsec

--- compiler/src/Language/Bond/Codegen/CustomMapping.hs
+++ compiler/src/Language/Bond/Codegen/CustomMapping.hs
@@ -1,7 +1,9 @@
 -- Copyright (c) Microsoft. All rights reserved.
 -- Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-{-# LANGUAGE OverloadedStrings #-}
+--{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies     #-}
 
 module Language.Bond.Codegen.CustomMapping
     ( AliasMapping(..)
@@ -15,6 +17,7 @@ import Data.Char
 import Control.Applicative
 import Prelude
 import Text.Megaparsec hiding (many, optional, (<|>))
+import Text.Megaparsec.Error (Dec)
 import Language.Bond.Syntax.Types
 
 -- | Specification of a fragment of type alias mappings.
@@ -34,12 +37,12 @@ data NamespaceMapping = NamespaceMapping
     , toNamespace :: QualifiedName      -- ^ namespace in the generated code
     }
 
-type Parser a = Parsec SourceName () a
+type Parser = Parsec Dec String
 
 whitespace :: Parser String
 whitespace = many (char ' ') <?> "whitespace"
 identifier :: Parser String
-identifier = many1 (alphaNum <|> char '_') <?> "identifier"
+identifier = some (alphaNumChar <|> char '_') <?> "identifier"
 qualifiedName :: Parser [String]
 qualifiedName = sepBy1 identifier (char '.') <?> "qualified name"
 symbol :: String -> Parser String
@@ -47,7 +50,7 @@ symbol s = whitespace *> string s <* whitespace
 equal :: Parser String
 equal = symbol "="
 integer :: Parser Integer
-integer = decimal <$> many1 digit <?> "decimal number"
+integer = decimal <$> some digitChar <?> "decimal number"
   where
     decimal = foldl (\x d -> 10 * x + toInteger (digitToInt d)) 0
 
@@ -58,12 +61,12 @@ integer = decimal <$> many1 digit <?> "decimal number"
 --
 -- > > parseAliasMapping "Example.OrderedSet=SortedSet<{0}>"
 -- > Right (AliasMapping {aliasName = ["Example","OrderedSet"], aliasTemplate = [Fragment "SortedSet<",Placeholder 0,Fragment ">"]})
-parseAliasMapping :: String -> Either ParseError AliasMapping 
+parseAliasMapping:: String -> Either (ParseError Char Dec) AliasMapping
 parseAliasMapping s = parse aliasMapping s s
   where
-    aliasMapping = AliasMapping <$> qualifiedName <* equal <*> many1 (placeholder <|> fragment) <* eof
+    aliasMapping = AliasMapping <$> qualifiedName <* equal <*> some (placeholder <|> fragment) <* eof
     placeholder = Placeholder <$> fromIntegral <$> between (char '{') (char '}') integer
-    fragment = Fragment <$> many1 (noneOf "{")
+    fragment = Fragment <$> some (noneOf "{")
 
 -- | Parse a namespace mapping specification used in command-line arguments of 
 -- <https://microsoft.github.io/bond/manual/compiler.html#command-line-options gbc>.
@@ -72,7 +75,7 @@ parseAliasMapping s = parse aliasMapping s s
 --
 -- > > parseNamespaceMapping "bond=Microsoft.Bond"
 -- > Right (NamespaceMapping {fromNamespace = ["bond"], toNamespace = ["Microsoft","Bond"]})
-parseNamespaceMapping :: String -> Either ParseError NamespaceMapping
+parseNamespaceMapping :: String -> Either (ParseError Char Dec) NamespaceMapping
 parseNamespaceMapping s = parse namespaceMapping s s
   where
     namespaceMapping = NamespaceMapping <$> qualifiedName <* equal <*> qualifiedName
